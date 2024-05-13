fe98deb758ecbca2639556085800233b070bc7f2
규현 <98933736+parkkyuhyeun@users.noreply.github.com>
2024-05-13 11:10:50 +0900

    Feat. Main Logic

--- Re_Chicaku/Assets/02_Scripts/Player.cs
+++ Re_Chicaku/Assets/02_Scripts/Player.cs
@@ -7,11 +7,41 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] GameObject hand;
-    [SerializeField] GameObject head;
+    [SerializeField] GameObject hand2;
+
+    private Rigidbody rigid;
+
+    private Vector3 playerTrm;
+    private Quaternion rArmAng;
+    private Quaternion lArmAng;
+
+    public float power;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
 
     void Update()
     {
         RotateToMouseDir();
+
+        Fire();
+
+        playerTrm = transform.position;
+        rArmAng = hand.transform.rotation;
+        lArmAng = hand2.transform.rotation;
+    }
+
+    void Fire()
+    {
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward * 10f);
+        if (Input.GetMouseButton(0))
+        {
+            //rigid.AddForce(-mouseWorldPosition * power);
+            Vector3 getVal = new Vector3(-mouseWorldPosition.x, -mouseWorldPosition.y, -mouseWorldPosition.z) * power;
+            rigid.velocity = getVal;
+        }
     }
 
     void RotateToMouseDir()
@@ -23,18 +53,29 @@ public class Player : MonoBehaviour
             hand.transform.position.x - mouseWorldPosition.x) * Mathf.Rad2Deg;
 
         float final = -(angle + 90f);
-        Debug.Log(angle + " / " + final);
 
-        hand.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, -final));
+        hand.transform.rotation = Quaternion.Euler(new Vector3(rArmAng.x, rArmAng.y, -final));
+
+        float angle2 = Mathf.Atan2(
+            hand2.transform.position.y - mouseWorldPosition.y,
+            hand2.transform.position.x - mouseWorldPosition.x) * Mathf.Rad2Deg;
+
+        float final2 = -(angle2 + 90f);
+
+        hand2.transform.rotation = Quaternion.Euler(new Vector3(lArmAng.x, lArmAng.y, -final2));
 
 
-        if(mouseWorldPosition.x > 0f)
+        if (mouseWorldPosition.x > playerTrm.x)
         {
-            transform.rotation = Quaternion.Euler(new Vector3(0f, 90f, 0f));
+            transform.localScale = new Vector3(1, 1, -1);
+            hand.transform.localScale = new Vector3(-1, 1, -1);
+            hand2.transform.localScale = new Vector3(1, 1, 1);
         }
         else
         {
-            transform.rotation = Quaternion.Euler(new Vector3(0f, -90f, 0f));
+            transform.localScale = new Vector3(1, 1, 1);
+            hand.transform.localScale = new Vector3(1, 1, 1);
+            hand2.transform.localScale = new Vector3(-1, 1, -1);
         }
     }
 }
